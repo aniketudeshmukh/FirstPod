63eabd1aa9e4a35795b83d965d9e433ec2e9b259
Aniket Deshmukh <aniket.deshmukh@capgemini.com>
2018-08-24 17:38:23 +0530

    Added Some Code

--- Example/FirstPod/ViewController.swift
+++ Example/FirstPod/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import FirstPod
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let pod = FirstPod()
+        pod.display(text: "ABC")
     }
 
     override func didReceiveMemoryWarning() {
---
+++ FirstPod.swift
@@ -0,0 +1,21 @@
+//
+//  FirstPod.swift
+//  FirstPod
+//
+//  Created by Aniket Deshmukh on 24/08/18.
+//
+
+import Foundation
+
+public class FirstPod {
+    public func display(text: String) {
+        print(text)
+    }
+}
+
+
+extension String {
+    public func show() {
+        print(self)
+    }
+}
